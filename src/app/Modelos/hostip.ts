bff272b7a223df174e9fdc27e08a3bcd9f07f62a
BryhanMG <eddyforte@hotmail.com>
2019-07-21 13:32:27 -0600

    commit de prueba 5

--- src/app/Modelos/hostip.ts
+++ src/app/Modelos/hostip.ts
@@ -1,3 +1,3 @@
-export const Hostip = 'localhost:27017';
+export const Hostip = 'moga-registry.herokuapp.com:3100';
 //export const Hostip = 'localhost:3100';
 //export const Hostip = '192.168.1.8';
